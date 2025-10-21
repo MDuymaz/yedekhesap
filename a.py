16123d28ce39431655cebaa095d17b8cc4fcc12c
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-10-21 09:36:25 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://dizipal1066.com/"
+url = "https://diziyiizle1.com/"
 headers = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -9,7 +9,7 @@ headers = {
     ),
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Referer": "https://dizipal1066.com/",
+    "Referer": "https://diziyiizle1.com/",
 }
 
 try:
