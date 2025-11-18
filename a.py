923a8c1329b5e06a541868ceb7596576e4242553
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-11-18 14:11:17 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,12 +1,35 @@
 import requests
+import re
 
-url = "https://tvplus.com.tr/canli-tv/yayin-akisi/atv-hd--124"
+url = "https://www.youtube.com/watch?v=pfQeMtSBv_Y"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    "referer": "https://www.youtube.com/@sifirtv/featured",
+    "sec-ch-ua": '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+    "sec-ch-ua-arch": "x86",
+    "sec-ch-ua-bitness": "64",
+    "sec-ch-ua-form-factors": '"Desktop"',
+    "sec-ch-ua-full-version": "141.0.7390.77",
+    "sec-ch-ua-full-version-list": '"Google Chrome";v="141.0.7390.77", "Not?A_Brand";v="8.0.0.0", "Chromium";v="141.0.7390.77"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": '""',
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-ch-ua-platform-version": '"10.0.0"',
+    "sec-ch-ua-wow64": "?0",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
 }
 
 response = requests.get(url, headers=headers)
 
-print("Status:", response.status_code)
-print(response.text)
+if response.status_code == 200:
+    # Sayfa içinde m3u8 URL'lerini ara
+    m3u8_links = re.findall(r'https?://[^\s"\']+\.m3u8', response.text)
+    if m3u8_links:
+        print("Bulunan m3u8 linkleri:\n")
+        for link in m3u8_links:
+            print(link)
+    else:
+        print("Herhangi bir .m3u8 linki bulunamadı.")
+else:
+    print(f"İstek başarısız oldu. Durum kodu: {response.status_code}")
