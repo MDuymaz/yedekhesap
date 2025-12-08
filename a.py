a7d8481fd61184ac9e487c0493130e1078862c39
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-12-08 12:17:30 +0300

    Update a.py

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ import re
 import json
 
 # Hedef URL
-url = "https://nl.pornhub.com/view_video.php?viewkey=67b090c219858"
+url = ""
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
