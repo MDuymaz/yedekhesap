42db984a303d908680a82db045d5b8544134d237
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2026-01-09 18:26:57 +0300

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,37 @@
+import os
+import requests
+
+GITHUB_TOKEN = os.getenv("GH_TOKEN")
+OWNER = "opitiopiti"
+
+REPOS = [
+    "Py",
+    "ordanburdansurdan",
+    "xtreamsistemi",
+    "domain",
+    "iptv-proxy-full",
+    "iptv-proxy-worker",
+    "m3u8file"
+
+]
+
+BRANCH = "main"
+
+headers = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+
+for repo in REPOS:
+    url = f"https://api.github.com/repos/{OWNER}/{repo}/zipball/{BRANCH}"
+    zip_name = f"{repo}.zip"
+
+    response = requests.get(url, headers=headers, stream=True)
+
+    if response.status_code == 200:
+        with open(zip_name, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"✅ {repo} indirildi.")
+    else:
+        print(f"❌ {repo} hata:", response.status_code)
